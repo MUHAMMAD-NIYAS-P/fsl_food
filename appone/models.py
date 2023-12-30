2ae0ec9d4e8f2a2a01af62d66e6b386baa81b855
MUHAMMAD NIYAS P <mohdniyasp98@email.com>
2023-12-30 18:50:24 +0530

    MenuItem model creation, apps added to settings

--- appone/models.py
+++ appone/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='menu_images/')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
